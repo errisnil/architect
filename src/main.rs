84dd91af33a3a791e147e44bd50a2e50669aa1f8
Err IsNil <errisnil@gmail.com>
2023-01-11 03:45:14 +0530

    tests in progress

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use native_tls::{Certificate, TlsConnector};
 use postgres::{Client, NoTls};
 use postgres_native_tls::MakeTlsConnector;
 use serde::Deserialize;
+use sqlparser::dialect::PostgreSqlDialect;
 
 #[derive(Deserialize, Default)]
 struct Config {
@@ -33,19 +34,6 @@ impl Config {
                 return Err(anyhow::anyhow!("couldn't read users home directory"));
             }
         };
-
-        // if self.passfile.is_empty() {
-        //     let passfile = if let Ok(v) = std::env::var("PGPASSFILE") {
-        //         v
-        //     } else {
-        //         "".to_owned()
-        //     };
-        //     if passfile.is_empty() {
-        //         self.passfile = format!("{:?}/.pgpass", &home_dir);
-        //     } else {
-        //         self.passfile = passfile;
-        //     }
-        // }
 
         if self.password.is_empty() {
             if let Ok(v) = std::env::var("PGPASSWORD") {
@@ -123,22 +111,25 @@ impl Config {
     }
 
     fn init(&mut self) -> Result<(Client, i64)> {
-        let _ = self.assert()?;
+        self.assert()?;
         let mut client = self.connect()?;
         client.execute(
             "
             CREATE TABLE IF NOT EXISTS schema_migrations (
                 version BIGINT PRIMARY KEY,
-                dirty BOOLEAN DEAFULT FALSE
+                dirty BOOLEAN DEFAULT FALSE
             )
         ",
             &[],
         )?;
         let mut last_version: i64 = 0;
-        for row in client.query(
+        if let Some(row) = (client.query(
             "SELECT version, dirty FROM schema_migrations ORDER BY version DESC LIMIT 1",
             &[],
-        )? {
+        )?)
+        .into_iter()
+        .next()
+        {
             let version: i64 = row.get(0);
             let dirty: bool = row.get(1);
             if dirty {
@@ -147,18 +138,17 @@ impl Config {
                 ));
             }
             last_version = version;
-            break;
         }
         Ok((client, last_version))
     }
 
-    fn dir(&self, parent: &std::path::PathBuf) -> Result<std::path::PathBuf> {
+    fn dir(&self, parent: &std::path::Path) -> Result<std::path::PathBuf> {
         let mig_path = parent.join(&self.app);
         if mig_path.exists() && !mig_path.is_dir() {
             return Err(anyhow::anyhow!(format!("invalid path: {:?}", &mig_path)));
         }
         if !mig_path.exists() {
-            let _ = std::fs::create_dir_all(&mig_path)?;
+            std::fs::create_dir_all(&mig_path)?;
         }
         Ok(mig_path)
     }
@@ -168,22 +158,27 @@ struct Migrator {
     config: Config,
     dir: std::path::PathBuf,
     last_version: i64,
-    client: Option<Client>,
-    versions_available: Vec<i64>,
+    client: Client,
+    versions_up: Vec<i64>,
+    versions_down: Vec<i64>,
     initialized: bool,
 }
 
 impl Migrator {
-    fn new(config: Config, dir: std::path::PathBuf) -> Result<Self> {
+    fn new(mut config: Config, dir: std::path::PathBuf) -> Result<Self> {
+        let dir = config.dir(&dir)?;
+        let (client, last_version) = config.init()?;
         let mut m = Migrator {
             config,
             dir,
-            last_version: 0,
-            client: None,
-            versions_available: Vec::<i64>::new(),
+            last_version,
+            client,
+            versions_up: Vec::<i64>::new(),
+            versions_down: Vec::<i64>::new(),
             initialized: false,
         };
-        m.init()?;
+        m.initialized = true;
+        m.available_versions()?;
         Ok(m)
     }
 
@@ -192,45 +187,54 @@ impl Migrator {
             return Err(anyhow::anyhow!("Migrator not initialized"));
         }
 
-        let reg = regex::Regex::new(r"^[1-9][0-9]*_(up|down)\.sql$")?;
+        let reg = regex::Regex::new(r"^([1-9][0-9]*)_(up|down)\.sql$")?;
+        let mut vup = Vec::<i64>::new();
+        let mut vdown = Vec::<i64>::new();
         for f in std::fs::read_dir(&self.dir)? {
             let f = f?;
-            let f = format!("{:?}", f.file_name());
+            let f = if let Some(v) = f.file_name().to_str() {
+                String::from(v)
+            } else {
+                eprintln!("osstring to str failed");
+                continue;
+            };
             let caps = if let Some(v) = reg.captures(&f) {
                 v
             } else {
                 continue;
             };
-            let mut ver = "".to_owned();
-            if let Some(v) = caps.get(1) {
-                ver = String::from(v.as_str());
-            } else {
-                continue;
-            }
-            if let Some(v) = caps.get(2) {
-                ver = format!("{}{}", &ver, v.as_str());
-            } else {
-                continue;
-            }
 
-            match ver.parse::<i64>() {
-                Ok(v) => self.versions_available.push(v),
+            let version = match caps.get(1) {
+                Some(v) => String::from(v.as_str()),
+                None => {
+                    continue;
+                }
+            };
+            let direction = match caps.get(2) {
+                Some(v) => String::from(v.as_str()),
+                None => {
+                    continue;
+                }
+            };
+
+            match version.parse::<i64>() {
+                Ok(v) => {
+                    if &direction == "up" {
+                        vup.push(v);
+                    } else if &direction == "down" {
+                        vdown.push(v);
+                    }
+                }
                 Err(e) => {
                     eprintln!("{:?}", e);
                 }
             }
         }
+        self.versions_up = vup;
+        self.versions_down = vdown;
+        self.versions_up.sort();
+        self.versions_down.sort();
 
-        Ok(())
-    }
-
-    fn init(&mut self) -> Result<()> {
-        let (client, last_version) = self.config.init()?;
-        self.client = Some(client);
-        self.last_version = last_version;
-        self.dir = self.config.dir(&self.dir)?;
-        self.initialized = true;
-        self.available_versions()?;
         Ok(())
     }
 
@@ -249,8 +253,198 @@ impl Migrator {
         }
         let _ = std::fs::File::create(&down)?;
         self.available_versions()?;
-        println!("Versions available:\n{:?}", self.versions_available);
         Ok(())
+    }
+
+    fn get_queries(&self, version: i64, direction: &str) -> Result<Vec<String>> {
+        let mut result = Vec::<String>::new();
+
+        let filename = self.dir.join(format!("{}_{}.sql", &version, &direction));
+        if !filename.exists() {
+            return Err(anyhow::anyhow!(format!(
+                "migration: \"{}_{}.sql\" does not exist",
+                &version, &direction
+            )));
+        }
+
+        let f = match filename.to_str() {
+            Some(v) => v.to_owned(),
+            None => {
+                return Err(anyhow::anyhow!("failed to get &str from PathBuf"));
+            }
+        };
+
+        let s = std::fs::read_to_string(&f)?;
+        let dialect = sqlparser::dialect::PostgreSqlDialect {};
+        let ast = sqlparser::parser::Parser::parse_sql(&dialect, &s)?;
+        for v in ast.iter() {
+            result.push(v.to_string());
+        }
+        if direction == "up" {
+            result.push(format!(
+                "INSERT INTO schema_migrations(version) VALUES ({})",
+                version
+            ));
+        } else if direction == "down" {
+            result.push(format!(
+                "DELETE FROM schema_migrations WHERE version = {}",
+                version,
+            ))
+        }
+
+        Ok(result)
+    }
+
+    fn run_migration(&mut self, version: i64, direction: String) -> Result<()> {
+        eprintln!("run_migration called");
+        let queries = self.get_queries(version, &direction)?;
+        let mut t = self.client.transaction()?;
+        for query in queries {
+            t.batch_execute(&query)?;
+        }
+        t.commit()?;
+        Ok(())
+    }
+
+    fn migrate_up_n(&mut self, n: usize, test: bool) -> Result<usize> {
+        if self.versions_up.is_empty() {
+            return Err(anyhow::anyhow!("no migrations found"));
+        }
+
+        let mut version = self.last_version;
+        let mut versions = Vec::<i64>::new();
+        let mut count = 0;
+        for v in self.versions_up.iter() {
+            if *v > version && count < n {
+                versions.push(*v);
+                version = *v;
+                count += 1;
+            }
+        }
+
+        for v in versions.iter() {
+            if !test {
+                match self.run_migration(*v, "up".to_owned()) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        return Err(anyhow::anyhow!(format!(
+                            "error running migration {}_up.sql",
+                            *v
+                        )));
+                    }
+                }
+            }
+            self.last_version = *v;
+        }
+
+        Ok(versions.len())
+    }
+
+    fn migrate_up(&mut self, test: bool) -> Result<usize> {
+        if self.versions_up.is_empty() {
+            return Err(anyhow::anyhow!("no migrations found"));
+        }
+
+        let mut version = self.last_version;
+        let mut versions = Vec::<i64>::new();
+        for v in self.versions_up.iter() {
+            if *v > version {
+                versions.push(*v);
+                version = *v;
+            }
+        }
+
+        for v in versions.iter() {
+            if !test {
+                match self.run_migration(*v, "up".to_owned()) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        return Err(anyhow::anyhow!(format!(
+                            "error running migration {}_up.sql",
+                            *v
+                        )));
+                    }
+                }
+            }
+            self.last_version = *v;
+        }
+        Ok(versions.len())
+    }
+
+    fn migrate_down_n(&mut self, n: usize, test: bool) -> Result<usize> {
+        if self.versions_down.is_empty() {
+            return Err(anyhow::anyhow!("no migrations found"));
+        }
+
+        if n == 0 {
+            return Err(anyhow::anyhow!("0 steps requested"));
+        }
+
+        let mut version = self.last_version;
+        let mut versions = Vec::<i64>::new();
+        let mut count = 0;
+        for (i, v) in self.versions_down.iter().enumerate() {
+            // Prepending
+            if *v < version && count < n {
+                versions.splice(0..0, [*v].iter().cloned());
+                version = *v;
+                count += 1;
+            }
+        }
+
+        for v in versions.iter() {
+            if !test {
+                match self.run_migration(*v, "up".to_owned()) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        return Err(anyhow::anyhow!(format!(
+                            "error running migration {}_up.sql",
+                            *v
+                        )));
+                    }
+                }
+            }
+            self.last_version = *v;
+        }
+
+        Ok(versions.len())
+    }
+
+    fn migrate_down(&mut self, test: bool) -> Result<usize> {
+        if self.versions_down.is_empty() {
+            return Err(anyhow::anyhow!("no migrations found"));
+        }
+
+        let mut version = self.last_version;
+        let mut versions = Vec::<i64>::new();
+        for v in self.versions_down.iter() {
+            // Prepending
+            if *v < version {
+                versions.splice(0..0, [*v].iter().cloned());
+                version = *v;
+            }
+        }
+
+        for v in versions.iter() {
+            if !test {
+                match self.run_migration(*v, "up".to_owned()) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        return Err(anyhow::anyhow!(format!(
+                            "error running migration {}_up.sql",
+                            *v
+                        )));
+                    }
+                }
+            }
+            self.last_version = *v;
+        }
+
+        Ok(versions.len())
     }
 }
 
@@ -278,13 +472,15 @@ fn main() -> Result<()> {
     let dir = std::path::PathBuf::from(&args.migdir);
 
     let m = Migrator::new(config, dir)?;
-    println!("{:?}", &m.versions_available);
+    println!("versions up:\n{:?}", &m.versions_up);
+    println!("versions down:\n{:?}", &m.versions_down);
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+    use anyhow::Result;
     use std::sync::Once;
     static INIT: Once = Once::new();
 
@@ -294,24 +490,21 @@ mod tests {
         });
     }
 
+    fn test_config() -> Result<crate::Config> {
+        let p = std::env::var("PGMIG_TEST_CONFIG")?;
+        let s = std::fs::read_to_string(p)?;
+        let c: crate::Config = toml::from_str(&s)?;
+        Ok(c)
+    }
+
     #[test]
     fn new_migration() {
         init();
-
-        let cs = r##"
-        app = "test"
-        dbname = "testpgmig"
-        host = "localhost"
-        port = 26257
-        user = "testadmin"
-        password = "testpass"
-        ssl = true
-        sslrootcert = "/Users/errisnil/code/cockroach/certs/ca.crt"
-        "##;
-        let config: crate::Config = toml::from_str(&cs).unwrap();
-        let mut m = crate::Migrator::new(config, std::path::PathBuf::from("./migrations")).unwrap();
+        let config = test_config().unwrap();
+        let mut m =
+            crate::Migrator::new(config, std::path::PathBuf::from("./new_migrations")).unwrap();
         m.new_migration().unwrap();
-        let files = std::fs::read_dir("./migrations/test").unwrap();
+        let files = std::fs::read_dir("./new_migrations/test").unwrap();
         let mut count = 0;
         let mut up_exists = false;
         let mut down_exists = false;
@@ -326,9 +519,169 @@ mod tests {
                 down_exists = true;
             }
         }
-        let _ = std::fs::remove_dir_all("./migrations/test");
+        let _ = std::fs::remove_dir_all("./new_migrations");
         assert_eq!(count, 2);
-        assert_eq!(up_exists, true);
-        assert_eq!(down_exists, true);
+        assert!(up_exists);
+        assert!(down_exists);
+    }
+
+    #[test]
+    fn available_versions() {
+        init();
+        let config = test_config().unwrap();
+        let mut m = crate::Migrator::new(config, std::path::PathBuf::from("./avvers")).unwrap();
+        const N: usize = 5;
+        for _ in 0..N {
+            m.new_migration().unwrap();
+        }
+        let _ = std::fs::remove_dir_all("./avvers");
+        assert_eq!(m.versions_up.len(), N);
+        assert_eq!(m.versions_down.len(), N);
+    }
+
+    #[test]
+    fn run_mig() {
+        init();
+        let config = test_config().unwrap();
+        let mut m =
+            crate::Migrator::new(config, std::path::PathBuf::from("./run_migrations")).unwrap();
+        m.new_migration().unwrap();
+        let version = *m.versions_up.last().unwrap();
+        let f = m.dir.join(format!("{}_up.sql", version));
+        std::fs::write(
+            &f,
+            b"
+CREATE TABLE IF NOT EXISTS __data__(
+    id INT PRIMARY KEY,
+    name VARCHAR(20) DEFAULT ''
+);
+        CREATE TABLE IF NOT EXISTS __another__(
+    id INT PRIMARY KEY,
+    name VARCHAR(20) DEFAULT ''
+);",
+        )
+        .unwrap();
+
+        let fdown = m.dir.join(format!("{}_down.sql", version));
+        std::fs::write(
+            &fdown,
+            b"
+DROP TABLE IF EXISTS __data__;
+DROP TABLE IF EXISTS __another__;",
+        )
+        .unwrap();
+
+        m.run_migration(version, "up".to_owned()).unwrap();
+
+        let vrows = m
+            .client
+            .query(
+                r"SELECT version FROM schema_migrations WHERE version = $1",
+                &[&version],
+            )
+            .unwrap();
+
+        let rows = m.client.query(r"SHOW TABLES", &[]).unwrap();
+
+        let mut count = 0;
+        for row in rows.iter() {
+            let v: &str = row.get(1);
+            if v == "__another__" || v == "__data__" {
+                count += 1;
+            }
+        }
+        let mver: i64 = vrows.first().unwrap().get(0);
+        assert_eq!(mver, version);
+        assert_eq!(count, 2);
+
+        m.run_migration(version, "down".to_owned()).unwrap();
+        let vrows = m
+            .client
+            .query(
+                r"SELECT version FROM schema_migrations WHERE version = $1",
+                &[&version],
+            )
+            .unwrap();
+
+        let rows = m.client.query(r"SHOW TABLES", &[]).unwrap();
+        let mut count = 0;
+        for row in rows.iter() {
+            let v: &str = row.get(1);
+            if v == "__another__" || v == "__data__" {
+                count += 1;
+            }
+        }
+        let _ = std::fs::remove_dir_all("./run_migrations");
+
+        assert_eq!(vrows.len(), 0);
+        assert_eq!(count, 0);
+    }
+
+    #[test]
+    fn mig_up_n() {
+        init();
+        let config = test_config().unwrap();
+        let mut m = crate::Migrator::new(config, std::path::PathBuf::from("./mig_up_n")).unwrap();
+        const N: usize = 15;
+        for _ in 0..N {
+            m.new_migration().unwrap();
+        }
+        let v = *m.versions_up.get(10).unwrap();
+        let n = m.migrate_up_n(11, true).unwrap(); // call with test true to not run migrations
+
+        let _ = std::fs::remove_dir_all("./mig_up_n");
+
+        assert_eq!(v, m.last_version);
+        assert_eq!(n, 11);
+    }
+    #[test]
+    fn mig_up() {
+        init();
+        let config = test_config().unwrap();
+        let mut m = crate::Migrator::new(config, std::path::PathBuf::from("./mig_up")).unwrap();
+        const N: usize = 15;
+        for _ in 0..N {
+            m.new_migration().unwrap();
+        }
+        let n = m.migrate_up(true).unwrap(); // call with test true to not run migrations
+
+        let _ = std::fs::remove_dir_all("./mig_up");
+
+        assert_eq!(Some(&m.last_version), m.versions_up.last());
+        assert_eq!(n, N);
+    }
+    #[test]
+    fn mig_down_n() {
+        init();
+        let config = test_config().unwrap();
+        let mut m = crate::Migrator::new(config, std::path::PathBuf::from("./mig_down_n")).unwrap();
+        const N: usize = 15;
+        for _ in 0..N {
+            m.new_migration().unwrap();
+        }
+        m.last_version = *m.versions_up.last().unwrap();
+        let n = m.migrate_down_n(5, true).unwrap(); // call with test true to not run migrations
+
+        let _ = std::fs::remove_dir_all("./mig_down_n");
+
+        assert_eq!(Some(&m.last_version), m.versions_down.get(9));
+        assert_eq!(n, 5);
+    }
+    #[test]
+    fn mig_down() {
+        init();
+        let config = test_config().unwrap();
+        let mut m = crate::Migrator::new(config, std::path::PathBuf::from("./mig_down_n")).unwrap();
+        const N: usize = 15;
+        for _ in 0..N {
+            m.new_migration().unwrap();
+        }
+        m.last_version = *m.versions_up.last().unwrap();
+        let n = m.migrate_down(true).unwrap(); // call with test true to not run migrations
+
+        let _ = std::fs::remove_dir_all("./mig_down_n");
+
+        assert_eq!(Some(&m.last_version), m.versions_down.get(9));
+        assert_eq!(n, 5);
     }
 }
